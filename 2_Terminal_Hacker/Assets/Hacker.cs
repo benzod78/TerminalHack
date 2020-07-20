351546674c072a068529f0645ea901a9d426f2ad
benzod78 <benzod78@gmail.com>
2020-07-19 20:39:33 -0400

    .

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -7,7 +7,15 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowMainMenu(); 
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.WriteLine("What would you like to HACK today?");
+        Terminal.WriteLine("Press 1 to hack the local library.");
+        Terminal.WriteLine("Press 2 to hack the local police station.");
+        Terminal.WriteLine("Enter your selection: ");
     }
 
     // Update is called once per frame
